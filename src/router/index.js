303570614fefada5cc9e19510822a7f63a6b873b
周磊 <5502655+zhoulei19930524@user.noreply.gitee.com>
2020-06-19 17:00:17 +0800

    login 路由重定向

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
+/* eslint-disable */
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    redirect: "login",
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: import("../views/Login/index.vue")
   },
   {
     path: "/about",
