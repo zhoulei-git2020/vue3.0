bfce46f861d8c6dc4cb5abecef5fd9ae4dbb2d43
周磊 <5502655+zhoulei19930524@user.noreply.gitee.com>
2020-06-20 17:25:18 +0800

    longin 页面开发

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,18 +1,66 @@
 <template>
   <div id="login">
-      这是登陆页面
+      <div class = "login-warp">
+          <ul class="menu-tab">
+              <li class="current" v-for="item in menuTab" :key="item.id" >{{item.txt}}</li>
+          </ul>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
+    name:'login',
+   
+   //数据
+   data(){
+        return{
+            menuTab:[
+                {txt:'登陆'},
+                {txt:'注册'}
+            ]
+        }
+    },
 
+    //创建完成时
+    created(){
+
+    },
+
+    //挂载完成时
+    mounted(){
+
+    },
+
+    //方法 
+    methods:{
+
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-    #login {
+    #login {         
         height: 100vh;
        background-color: #344a5f; 
+    }
+    .login-warp{
+        width:330px;
+        margin: auto;
+    }
+    .menu-tab{
+       text-align: center; 
+       li{
+           display: inline-block;
+           width:88px;
+           line-height:36px ; 
+           font-size: 14px;
+           color: #fff;
+           border-radius: 2px;
+           cursor: pointer; //鼠标移动上面变成手势
+       }
+       .current{
+           background-color: rgba(0,0,0,.1);
+       }
     }
 </style>
