13343b87c66c670efaa9ee94c1c0e9052ed86ddb
zhoulei-git2020 <812515692@qq.com>
2020-07-14 20:28:41 +0800

    组件创建完成

---
+++ src/views/Layout/Component/Header.vue
@@ -0,0 +1,13 @@
+ADER<template>
+    <div>头部</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,10 +1,50 @@
 <template>
-    <div>11111</div>
+    <div>
+        <LayoutHeader/>
+        <LayoutMain/>
+        <LayoutNav/>
+    </div>
 </template>
 
 <script>
+import LayoutHeader from './Component/Header';
+import LayoutMain from './Component/Main';
+import LayoutNav from './Component/Nav'
 export default {
+    name:'layout',
+
+    //组件注册区
+    components:{
+        LayoutHeader,
+        LayoutMain,
+        LayoutNav
+    },
     
+
+    //数据区
+     data(){
+         return{
+
+         }
+     },
+
+
+    //创建完成时
+    created(){
+
+    },
+
+    //挂载完成时
+    mounted(){
+       
+    },
+
+    //方法
+    methods:{
+
+    }
+
+
 }
 </script>
 
